9408cef6ad8e8f80ef8da524eddbd5550ad3e3f7
liushooter <shooter.hero@gmail.com>
2020-12-02 01:12:44 +0800

    layout

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import {  Layout, Button  } from 'antd';
 import './App.css';
+
+const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
   return (
     <div className="App">
+      <Layout>
+        <Header>
+          <Button type="primary">Connect Wallet</Button>
+        </Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
+      </Layout>
     </div>
   );
 }
